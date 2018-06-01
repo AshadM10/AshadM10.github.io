68c6e64215ffb9955a1f2b81460631b8a479dae6
Ashadduzzaman Mollah <iftimollah@gmail.com>
2018-06-01 12:25:21 +0100

    new folder

---
+++ script.js
@@ -0,0 +1,43 @@
+$(document) .ready(function() {
+
+console.log("Hello");
+//alert("Your computer has a virus"); This is a comment! the computer will not run it
+console.log(1+1);
+
+var name = "Ashad";
+console.log(name);
+
+var lucky = 7;
+console.log("Seven times three is...");
+console.log(lucky * 3);
+
+
+if (1 + 1 === 2) {
+	console.log("Maths works");
+}
+
+if (1 + 2 === 1000) {
+	console.log("Oh dear");
+}
+
+var name = "Ashad";
+
+if (name === "Naeem") {
+	console.log("Your name is Naeem");
+} else {
+	console.log("Nope, your not Naeem");
+}
+
+$(document.body).append("Hellooooooooooo!");
+});
+
+function checkPassword() {
+var enteredText = $("#password").val();
+
+if (enteredText === "yellow") {
+	$(document.body).append("That was the password!");
+ } else {
+  $(document.body).append("That was not the password :(");
+ }
+
+}
